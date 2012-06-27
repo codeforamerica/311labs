e09f87748503a3f1b3569f3ef145cc1197762549
Jesse Bounds <jesse@rebounds.net>
2012-06-27 09:47:35 -0700

    updated links for 311labs domain

--- dist/debug/require.js
+++ dist/debug/require.js
@@ -316,7 +316,7 @@ var requirejs, require, define;
 this['JST'] = this['JST'] || {};
 
 this['JST']['app/templates/labs.html'] = function(data) { return function (obj,_) {
-var __p=[],print=function(){__p.push.apply(__p,arguments);};with(obj||{}){__p.push('<div id="test">\n<h1>this is labz</h1>\n\n<span>you like it?</span>\n<span>maybe you try click on links?</span><br />\n\n<br />\n<br />\n\n<li>\n<a href="http://dailybrief.herokuapp.com/boston">Boston DailyBrief-a-roo</a>\n</li><br />\n<li>\n<a href="http://dailybrief.herokuapp.com/baltimore">Balti MOAR!!!! brief</a>\n</li>\n\n<br />\n<br />\n<br />\n\n<h3>\n  <span>thank you for trying the briefs - send us the\n    <a href="http://www.forcefeedback.tv/"> feedback?</a></span>\n</h3>\n</div>\n');}return __p.join('');
+var __p=[],print=function(){__p.push.apply(__p,arguments);};with(obj||{}){__p.push('<div id="test">\n<h1>this is labz</h1>\n\n<span>you like it?</span>\n<span>maybe you try click on links?</span><br />\n\n<br />\n<br />\n\n<li>\n<a href="http://dailybrief.311labs.org/boston">Boston DailyBrief-a-roo</a>\n</li><br />\n<li>\n<a href="http://dailybrief.311labs.org/baltimore">Balti MOAR!!!! brief</a>\n</li>\n\n<br />\n<br />\n<br />\n\n<h3>\n  <span>thank you for trying the briefs - send us the\n    <a href="http://www.forcefeedback.tv/"> feedback?</a></span>\n</h3>\n</div>\n');}return __p.join('');
 }(data, _)};
 
 /*!
